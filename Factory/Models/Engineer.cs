cb41ea663bae9a41a8c25b3e05d25f66e86e0132
Maxwell Alvord <maxwellalvord@gmail.com>
2022-08-05 08:42:22 -0700

    add fix to model/ add corresponding join table logic

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -12,6 +12,6 @@ namespace Factory.Models
     public int EngineerId { get; set; }
     public string Description { get; set; }
 
-    public virtual ICollection<EngineerMachine> JoinEntities { get;}
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
   }
 }
